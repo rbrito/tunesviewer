12f9c83e44420229f1f3d0d7adf816b2b17935eb
Rogério Brito <rbrito@ime.usp.br>
2012-01-08 19:11:51 -0200

    testParser: Rework the way the tests work for future extensibility.

--- TunesViewer/usr/share/tunesviewer/testParser.py
+++ TunesViewer/usr/share/tunesviewer/testParser.py
@@ -1,23 +1,32 @@
+import logging
 import unittest
 import urllib2
+
 from Parser import Parser
 
 # Test the parser class - Requires network!
 class TestParser(unittest.TestCase):
-	def testParse(self):
-		opener = urllib2.build_opener()
-		opener.addheaders = [('User-agent', 'iTunes/10.5')]
+	def setUp(self):
+		"""
+		Take care of the preparation of the tests.
+		"""
+		self.o = urllib2.build_opener()
+		self.o.addheaders = [('User-agent', 'iTunes/10.5')]
+
+	# TODO: Add some more pages to test (especially XML pages)
+	def testParseAgriculture(self):
 		url = "http://itunes.apple.com/WebObjects/DZR.woa/wa/viewPodcast?cc=us&id=387961518"
-		text = opener.open(url).read()
-		parsed = Parser(url,"text/HTML",text)
-		assert parsed.Redirect == ''
-		assert parsed.Title == 'Food and Sustainable Agriculture'
-		assert len(parsed.mediaItems)==7
-		print "\n Important: Check this! \n"
-		for line in parsed.mediaItems:
-			print line
-		
-		#TODO: Probably should add some more pages to test, especially XML pages...
-	
+		text = self.o.open(url).read()
+		parsed_html = Parser(url, "text/HTML", text)
+
+		self.assertEqual(parsed_html.Redirect, '')
+		self.assertEqual(parsed_html.Title, 'Food and Sustainable Agriculture')
+		self.assertEqual(len(parsed_html.mediaItems), 7)
+
+		# The following should be made into proper tests
+		for line in parsed_html.mediaItems:
+			logging.warn(line)
+
 if __name__ == "__main__":
-	unittest.main() # run all tests
+	# run all tests
+	unittest.main()
