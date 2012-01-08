ab876645968fdb59591835667542a7bcd77483ce
Rogério Brito <rbrito@ime.usp.br>
2012-01-08 18:18:43 -0200

    Refactor the unittests for `common.py`

--- TunesViewer/usr/share/tunesviewer/testCommon.py
+++ TunesViewer/usr/share/tunesviewer/testCommon.py
@@ -1,19 +1,23 @@
+import logging
 import unittest
+
 from common import *
 
 class TestCommon(unittest.TestCase):
 	def testTimeFind(self):
-		assert timeFind(1000)=="00:01"
-		assert timeFind("bogus")=="bogus"
-		assert timeFind(1000*60)=="01:00"
-		
+		self.assertEqual(timeFind(1000), "00:01")
+		self.assertEqual(timeFind("bogus"), "bogus")
+		self.assertEqual(timeFind(1000*60), "01:00")
+
 	def testHTML(self):
-		assert htmlentitydecode("M&amp;M")=="M&M"
-		
+		self.assertEqual(htmlentitydecode("M&amp;M"), "M&M")
+
 	def testSafeFilename(self):
-		good = "/home/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 $%`-_@{}~!#()."
-		assert safeFilename("/home/mydirectory/somefile",True)=="somefile"
-		assert safeFilename(good,True)==good[6:] #without /home/.
-		
+		basename = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 $%`-_@{}~!#()."
+		good = "/home/" + basename
+		self.assertEqual(safeFilename("/home/mydirectory/somefile", True), "somefile")
+		self.assertEqual(safeFilename(good, True), good[6:]) #without /home/.
+
 if __name__ == "__main__":
-	unittest.main() # run all tests
+	# run all tests
+	unittest.main()
