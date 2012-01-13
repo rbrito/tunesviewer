70f6242203618fec6b944af9caba95eb4a616cc7
Rogério Brito <rbrito@ime.usp.br>
2012-01-13 07:58:59 -0200

    common: Rework the `timeFind` function for consist look with iTunes U.

--- tests/testCommon.py
+++ tests/testCommon.py
@@ -9,8 +9,16 @@ from common import *
 
 class TestCommon(unittest.TestCase):
 	def testTimeFind(self):
-		self.assertEqual(timeFind(1000), "00:01")
-		self.assertEqual(timeFind(1000*60), "01:00")
+		self.assertEqual(timeFind(1000), "0:01")
+		self.assertEqual(timeFind(1000*60), "1:00")
+		self.assertEqual(timeFind(1000*61), "1:01")
+		self.assertEqual(timeFind(1000*70), "1:10")
+		self.assertEqual(timeFind(1000*3599), "59:59")
+		self.assertEqual(timeFind(1000*3600), "1:00:00")
+		self.assertEqual(timeFind(1000*3601), "1:00:01")
+		self.assertEqual(timeFind(1000*3660), "1:01:00")
+		self.assertEqual(timeFind(1000*3661), "1:01:01")
+		self.assertEqual(timeFind(1000*7200), "2:00:00")
 
 		self.assertEqual(timeFind("bogus"), "bogus")
 		self.assertEqual(timeFind([]), [])
