2ba7995f3df82a945c2c7fe5c36398f18aff7909
Rogério Brito <rbrito@ime.usp.br>
2012-01-14 05:34:19 -0200

    testParser: cosmetics: Whitespace work.

--- tests/testParser.py
+++ tests/testParser.py
@@ -9,6 +9,7 @@ sys.path.append('../src')
 
 from Parser import Parser
 
+
 # Test the parser class - Requires network!
 class TestParser(unittest.TestCase):
 	def setUp(self):
@@ -17,6 +18,7 @@ class TestParser(unittest.TestCase):
 		"""
 		self.o = urllib2.build_opener()
 		self.o.addheaders = [('User-agent', 'iTunes/10.5')]
+
 
 	# TODO: Add some more pages to test (especially XML pages)
 	def testParseAgriculture(self):
@@ -32,6 +34,7 @@ class TestParser(unittest.TestCase):
 		for line in parsed_html.mediaItems:
 			logging.warn(line)
 
+
 	def testParseFHSU(self):
 		url = "http://deimos.apple.com/WebObjects/Core.woa/Browse/fhsu.edu"
 		text = self.o.open(url).read()
@@ -45,18 +48,24 @@ class TestParser(unittest.TestCase):
 		for line in parsed_html.mediaItems:
 			logging.warn(line)
 
+
 	def testPresidentHammond(self):
 		url = "http://deimos.apple.com/WebObjects/Core.woa/Browse/fhsu.edu.1152205441"
 		text = self.o.open(url).read()
 		parsed_html = Parser(url, "text/HTML", text)
 
 		self.assertEqual(parsed_html.Redirect, '')
-		self.assertEqual(parsed_html.Title, 'iTunes U > Fort Hays State University > FHSU News > From the President - President Hammond') # Maybe it could be smarter about finding the title...
+		# Maybe it could be smarter about finding the title...
+		self.assertEqual(parsed_html.Title,
+				 'iTunes U > Fort Hays State University > '
+				 'FHSU News > From the President - '
+				 'President Hammond')
 		self.assertEqual(len(parsed_html.mediaItems), 28)
 
 		# The following should be made into proper tests
 		for line in parsed_html.mediaItems:
 			logging.warn(line)
+
 
 	def testTopDownloads(self):
 		url = "https://deimos.apple.com/WebObjects/Core.woa/BrowsePrivately/georgefox.edu.01651902695"
@@ -70,6 +79,7 @@ class TestParser(unittest.TestCase):
 		for line in parsed_html.mediaItems:
 			logging.warn(line)
 
+
 	def testParseSIUC(self):
 		url = "http://deimos3.apple.com/WebObjects/Core.woa/Browse/siuc.edu?ignore.mscache=9669968"
 		text = self.o.open(url).read()
@@ -82,6 +92,7 @@ class TestParser(unittest.TestCase):
 		# The following should be made into proper tests
 		for line in parsed_html.mediaItems:
 			logging.warn(line)
+
 
 	def testParseSJSU(self):
 		url = "http://deimos3.apple.com/WebObjects/Core.woa/Browse/sjsu.edu?ignore.mscache=3176353"
@@ -103,9 +114,27 @@ class TestParser(unittest.TestCase):
 		parsed_html = Parser(url, "text/HTML", text)
 
 		self.assertEqual(parsed_html.Redirect, '')
-		self.assertEqual(parsed_html.Title, 'iTunes U > George Fox University > Chapel - Chapel 2011-2012')
+		self.assertEqual(parsed_html.Title,
+				 'iTunes U > George Fox University > Chapel - Chapel 2011-2012')
 		#Are all tabs shown?
-		assert parsed_html.tabMatches == [', Selected. Chapel 2011-2012', '. Shalom 2011-2012', '. Chapel 2010-2011', '. Shalom 2010-2011', '. Other 2010-2011', '. Chapel 2009-2010', '. Shalom 2009-2010', '. Chapel 2008-2009', '. Shalom 2008-2009', '. Chapel 2007 - 2008', '. Chapel 2006-2007', '. Chapel 2005-2006', '. Chapel 2004-2005', '. Chapel 2003-2004', '. Chapel 2002-2003', '. Chapel 2001-2002', '. Chapel 2000-2001']
+		assert parsed_html.tabMatches == [', Selected. Chapel 2011-2012',
+						  '. Shalom 2011-2012',
+						  '. Chapel 2010-2011',
+						  '. Shalom 2010-2011',
+						  '. Other 2010-2011',
+						  '. Chapel 2009-2010',
+						  '. Shalom 2009-2010',
+						  '. Chapel 2008-2009',
+						  '. Shalom 2008-2009',
+						  '. Chapel 2007 - 2008',
+						  '. Chapel 2006-2007',
+						  '. Chapel 2005-2006',
+						  '. Chapel 2004-2005',
+						  '. Chapel 2003-2004',
+						  '. Chapel 2002-2003',
+						  '. Chapel 2001-2002',
+						  '. Chapel 2000-2001']
+
 
 	def testWebRedirect(self):
 		url = "http://www2.ohlone.edu/cgi-bin/itunespub/itunes_public.pl"
